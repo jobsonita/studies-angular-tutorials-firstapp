d96e17329ff271fa64f787e6ae80d40a7a998061
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 01:50:51 +0000

    Lesson 11 - Step 2 - Get Route parameters
    https://angular.io/tutorial/first-app/first-app-lesson-11#step-2---get-route-parameters

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+
 
 @Component({
   selector: 'app-details',
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
+  template: `<p>details works! {{ housingLocationId }}</p>`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId?: number;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
