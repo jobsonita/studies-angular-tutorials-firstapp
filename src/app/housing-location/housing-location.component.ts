944cbdc0cf727a6dfa9912a67a508bfad90901f9
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 00:09:42 +0000

    Lesson 05 - Step 1 - Import the Input decorator
    https://angular.io/tutorial/first-app/first-app-lesson-05#step-1---import-the-input-decorator

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
