2d101b8816b47dbd6fdec31451d338f10fb76540
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 02:29:07 +0000

    Lesson 13 - Adding a search field
    https://angular.io/tutorial/first-app/first-app-lesson-13#lesson-13---add-the-search-feature-to-your-app
    
    Note: I used the submit event instead of the click event in order
    to also catch the enter key event.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,14 @@ import { HousingService } from '../housing.service';
   selector: 'app-home',
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+      <form (submit)="filterResults(filter.value); false">
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="submit">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation">
       </app-housing-location>
     </section>
@@ -25,7 +25,22 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
+  filteredLocationList: HousingLocation[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()) ||
+        housingLocation?.name.toLowerCase().includes(text.toLowerCase()) ||
+        housingLocation?.state.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
