a2cc43dc753111dd39b4175a90b163ae5293fe9b
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 01:27:57 +0000

    Lesson 10 - Add routes to the application
    https://angular.io/tutorial/first-app/first-app-lesson-10#lesson-10---add-routes-to-the-application
    
    Unfortunately, I was already using Angular's default Routing configuration,
    so the steps I followed were a bit different.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     HomeComponent,
+    DetailsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
