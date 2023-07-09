58cea24a320d3f2bf7daca355022ee55912e2593
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 01:10:22 +0000

    Lesson 09 - Step 2 - Add static data to service
    https://angular.io/tutorial/first-app/first-app-lesson-09#step-2---add-static-data-to-the-new-service

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,9 +1,121 @@
 import { Injectable } from '@angular/core';
 
+import { HousingLocation } from './housing-location';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: 'https://loremflickr.com/320/240/building?lock=1',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: 'https://loremflickr.com/320/240/building?lock=2',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: 'https://loremflickr.com/320/240/building?lock=3',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: 'https://loremflickr.com/320/240/building?lock=4',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: 'https://loremflickr.com/320/240/building?lock=5',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://loremflickr.com/320/240/building?lock=6',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://loremflickr.com/320/240/building?lock=7',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://loremflickr.com/320/240/building?lock=8',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://loremflickr.com/320/240/building?lock=9',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: 'https://loremflickr.com/320/240/building?lock=10',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ];
 
   constructor() { }
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
 }
