ff9f325921f2c653f1e224fdf0699473cdd0c2c2
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-09 02:14:08 +0000

    Lesson 12 - Step 3 - Adding a form to the app

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 import { HousingService } from '../housing.service';
@@ -23,17 +24,45 @@ import { HousingLocation } from '../housing-location';
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
     </article>
   `,
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
   route = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation?: HousingLocation;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,4 +118,8 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    window.alert(`Homes application received:\n\t- firstName: ${firstName},\n\t- lastName: ${lastName},\n\t- email: ${email}`);
+  }
 }
